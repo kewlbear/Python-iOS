c570d18cc15d6091399598e7df93ed754ecc7cc6
Changbeom Ahn <kewlbear@gmail.com>
2020-11-13 16:58:50 +0900

    extract Swift packages

--- Package.swift
+++ Package.swift
@@ -14,14 +14,12 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/rderik/Csqlite3", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "PythonKit", dependencies: [
-                    "CPython", "BZip2", "OpenSSL", "XZ",
-                    .product(name: "Csqlite3", package: "Csqlite3")]),
+                    "CPython", "BZip2", "OpenSSL", "XZ"]),
         .binaryTarget(name: "CPython", path: "Frameworks/CPython.xcframework"),
         .binaryTarget(name: "BZip2", path: "Frameworks/BZip2.xcframework"),
         .binaryTarget(name: "OpenSSL", path: "Frameworks/OpenSSL.xcframework"),
--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # Python-iOS
 
-A description of this package.
+This swift package enables you to use python modules in your iOS apps.
+
+## Installation
+
+```
+.package(url: "https://github.com/kewlbear/Python-iOS.git", from: "0.0.1")
+```
+
+## Usage
+
+```
+import CPython
+import Resources
+
+setenv("PYTHONOPTIMIZE", "1", 1)
+setenv("PYTHONDONTWRITEBYTECODE", "1", 1)
+setenv("PYTHONUNBUFFERED", "1", 1)
+
+guard let pythonHome = Resources.libURL?
+        .deletingLastPathComponent()
+        .path else { fatalError() }
+let wHome = Py_DecodeLocale(pythonHome, nil)
+Py_SetPythonHome(wHome)
+
+setenv("PYTHONPATH",
+       FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].path,
+       1)
+
+setenv("TMP", NSTemporaryDirectory(), 1)
+
+Py_Initialize()
+```
+
+## Credits
+
+This package uses pre-built version of libraries downloaded from https://github.com/beeware/Python-Apple-support.
+
+## License
+
+MIT
