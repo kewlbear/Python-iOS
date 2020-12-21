3fd96e60835d780f6893c64f013b7bdce7aeadef
Changbeom Ahn <kewlbear@gmail.com>
2020-12-21 14:16:07 +0900

    add arm64 ios simulator

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "Python-iOS",
-            targets: ["Symbols", "Python", "BZip2", "OpenSSL", "XZ", "Resources"]),
+            targets: ["Symbols", "Python", "BZip2", "SSL", "Crypto", "XZ", "Resources"]),
     ],
     dependencies: [
         .package(url: "https://github.com/alloyapple/CSqlite3.git", .branch("master")),
@@ -16,10 +16,11 @@ let package = Package(
     targets: [
         .binaryTarget(name: "Python", path: "Frameworks/Python.xcframework"),
         .binaryTarget(name: "BZip2", path: "Frameworks/BZip2.xcframework"),
-        .binaryTarget(name: "OpenSSL", path: "Frameworks/OpenSSL.xcframework"),
+        .binaryTarget(name: "SSL", path: "Frameworks/SSL.xcframework"),
+        .binaryTarget(name: "Crypto", path: "Frameworks/Crypto.xcframework"),
         .binaryTarget(name: "XZ", path: "Frameworks/XZ.xcframework"),
         .target(name: "Resources", dependencies: ["Symbols"], resources: [.copy("lib")]),
-        .target(name: "Symbols", dependencies: ["Python", "BZip2", "OpenSSL", "XZ", "CSqlite3", "Clibz"]),
+        .target(name: "Symbols", dependencies: ["Python", "BZip2", "SSL", "Crypto", "XZ", "CSqlite3", "Clibz"]),
         .target(name: "Clibz", linkerSettings: [LinkerSetting.linkedLibrary("z")]),
         .testTarget(
             name: "PythonTests",
