bdb0a8aa2cdd2fc3df70316802f6db2ca5d9d58d
Changbeom Ahn <kewlbear@gmail.com>
2021-02-24 17:06:34 +0900

    fix package resulution error

--- Package.swift
+++ Package.swift
@@ -8,19 +8,19 @@ let package = Package(
     products: [
         .library(
             name: "Python-iOS",
-            targets: ["Python", "SSL", "Crypto", "FFI", "PythonSupport"]),
+            targets: ["libpython3", "libssl", "libcrypto", "libffi", "PythonSupport"]),
     ],
     targets: [
-        .binaryTarget(name: "Python", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.1/libpython3.xcframework.zip", checksum: "245c51a97eda854a7b0c7bd507f24d1dfc2efae38f20aceac91fe0fd99a6eebe"),
-        .binaryTarget(name: "SSL", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.1/libssl.xcframework.zip", checksum: "1fc7dd3e95d5152812bc8d74450fdc45539e4ac53d4008b21b7f0a81d2fc52a9"),
-        .binaryTarget(name: "Crypto", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.1/libcrypto.xcframework.zip", checksum: "43948ae2aac97bf80445ad0e38dd943b4f903506802633bd82d8b813da0328bf"),
-        .binaryTarget(name: "FFI", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.1/libffi.xcframework.zip", checksum: "0b028a1068f5f085ba1861b73928b99d14970438639d2531c3f31afe4d0e0e3c"),
+        .binaryTarget(name: "libpython3", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.1/libpython3.xcframework.zip", checksum: "245c51a97eda854a7b0c7bd507f24d1dfc2efae38f20aceac91fe0fd99a6eebe"),
+        .binaryTarget(name: "libssl", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.1/libssl.xcframework.zip", checksum: "1fc7dd3e95d5152812bc8d74450fdc45539e4ac53d4008b21b7f0a81d2fc52a9"),
+        .binaryTarget(name: "libcrypto", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.1/libcrypto.xcframework.zip", checksum: "43948ae2aac97bf80445ad0e38dd943b4f903506802633bd82d8b813da0328bf"),
+        .binaryTarget(name: "libffi", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.1/libffi.xcframework.zip", checksum: "0b028a1068f5f085ba1861b73928b99d14970438639d2531c3f31afe4d0e0e3c"),
         .target(name: "PythonSupport",
                 dependencies: [
-                    "Python",
-                    "SSL",
-                    "Crypto",
-                    "FFI",
+                    "libpython3",
+                    "libssl",
+                    "libcrypto",
+                    "libffi",
                 ],
                 resources: [.copy("lib")],
                 linkerSettings: [
