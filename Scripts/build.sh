34b1ae28bd3c23cca8224c0010be66b27dfd281b
Changbeom Ahn <kewlbear@gmail.com>
2021-04-06 15:24:05 +0900

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,4 +2,4 @@ cd kivy-ios
 
 xcodebuild -create-xcframework -output libpython3.xcframework \
 	-library dist/frameworks/libpython3.xcframework/ios-arm64/libpython3.8.a -headers dist/root/python3/include/python3.8 \
-	-library dist/frameworks/libpython3.xcframework/ios-arm64_x86_64-simulator/libpython3.8.a -headers dist/root/python3/include/python3.8
+	-library dist/frameworks/libpython3.xcframework/ios-arm64_x86_64-simulator/libpython3.a -headers dist/root/python3/include/python3.8
