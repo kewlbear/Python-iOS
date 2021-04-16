f5333228b625560a90c17d9e2d3a9b2aac6967a9
kewlbear <kewlbear@users.noreply.github.com>
2021-04-16 07:16:30 +0000

    add 0.0.20210416071604

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
             ]),
     ],
     targets: [
-        .binaryTarget(name: "libpython3", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.20210412042656/libpython3.xcframework.zip", checksum: "85aba07c6d968f3b01c968c5f69d4b79ffde0dcbfa45265ed9288f262f3666b8"),
-        .binaryTarget(name: "libssl", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.20210412042656/libssl.xcframework.zip", checksum: "bafcc38597d44f6e4441a3649b1f050a0f50ecd6eae39ac30591022c0bcccfb8"),
-        .binaryTarget(name: "libcrypto", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.20210412042656/libcrypto.xcframework.zip", checksum: "7699064042890de1eac1fad45843fadea10739e73ae4f064ed19bfad28dcce2b"),
-        .binaryTarget(name: "libffi", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.20210412042656/libffi.xcframework.zip", checksum: "93d243e494d2d19f83581f1dadc1f33d51dd58d918e0cbf8c6d298d4f01bb98a"),
+        .binaryTarget(name: "libpython3", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.20210416071604/libpython3.xcframework.zip", checksum: "e04f8175549283c3a5af54e5997db3ab12e6dfecc278dedd04ed85c05c36283a"),
+        .binaryTarget(name: "libssl", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.20210416071604/libssl.xcframework.zip", checksum: "1e10930fbe511663e829cbf6f76554b44094ed3fcda7d136a9bf9dcf1849f2be"),
+        .binaryTarget(name: "libcrypto", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.20210416071604/libcrypto.xcframework.zip", checksum: "6837dda1876ad567202a827298774d3dbf21ad044a1d7d2cfcbd6cb2f45c1440"),
+        .binaryTarget(name: "libffi", url: "https://github.com/kewlbear/Python-iOS/releases/download/0.0.20210416071604/libffi.xcframework.zip", checksum: "d5af8d9f5824f6f08235a55f03b2d7d03e2992e2f5391137c897fbbd1889960d"),
         .target(name: "LinkPython",
                 dependencies: [
                     "libpython3",
