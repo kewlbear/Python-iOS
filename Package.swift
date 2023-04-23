f0efaf32ec2506ed2654051e5ab3dd06d4e78c29
kewlbear <kewlbear@users.noreply.github.com>
2023-04-23 09:03:23 +0000

    add v0.1.1-b20230423-090254

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
             ]),
     ],
     targets: [
-        .binaryTarget(name: "libpython3", url: "https://github.com/kewlbear/Python-iOS/releases/download/v0.1.1-b20230423-053002/libpython3.xcframework.zip", checksum: "ea0378113a1fbd12b3a7bbea43a99c13c85b997674e2b41590e6e63e625e784c"),
-        .binaryTarget(name: "libssl", url: "https://github.com/kewlbear/Python-iOS/releases/download/v0.1.1-b20230423-053002/libssl.xcframework.zip", checksum: "93f81d4b914c88a37cf2d3e391308488e8c514197d3e7bb482ca51b262a3a2d6"),
-        .binaryTarget(name: "libcrypto", url: "https://github.com/kewlbear/Python-iOS/releases/download/v0.1.1-b20230423-053002/libcrypto.xcframework.zip", checksum: "2565f50faed2320f0bb9fa564ac0c1c046697e4b7226e960cd0eb9115c678431"),
-        .binaryTarget(name: "libffi", url: "https://github.com/kewlbear/Python-iOS/releases/download/v0.1.1-b20230423-053002/libffi.xcframework.zip", checksum: "95656a003e34b2237f3712be55438cc0ee2f9bb7c523fd9b375a93ed912de579"),
+        .binaryTarget(name: "libpython3", url: "https://github.com/kewlbear/Python-iOS/releases/download/v0.1.1-b20230423-090254/libpython3.xcframework.zip", checksum: "fff9f3874a12b725ac3dce406658fc99e75d885cc4356a1f1637490f657ec816"),
+        .binaryTarget(name: "libssl", url: "https://github.com/kewlbear/Python-iOS/releases/download/v0.1.1-b20230423-090254/libssl.xcframework.zip", checksum: "e04b326f6779e567c5ba2f621f711762ddf7fb07f46eda5a5e70119836f5ca91"),
+        .binaryTarget(name: "libcrypto", url: "https://github.com/kewlbear/Python-iOS/releases/download/v0.1.1-b20230423-090254/libcrypto.xcframework.zip", checksum: "c0b46f250be2051b6f847c811d31f4ff742bb08a90fc8c266ef61ec48293a3db"),
+        .binaryTarget(name: "libffi", url: "https://github.com/kewlbear/Python-iOS/releases/download/v0.1.1-b20230423-090254/libffi.xcframework.zip", checksum: "c1ec3fe8a047b88809c74aba048737a3d05f86d61e061fcccfe970c872dbe6e5"),
         .target(name: "LinkPython",
                 dependencies: [
                     "libpython3",
@@ -28,7 +28,6 @@ let package = Package(
                 ],
                 linkerSettings: [
                     .linkedLibrary("z"),
-                    .linkedLibrary("bz2"),
                     .linkedLibrary("sqlite3"),
                 ]
         ),
